cf9905a6ca718bd67f46559e36c0e78c16def6db
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-02 17:57:10 +0530

    refactor Navbar component to use centralized asset imports for improved maintainability

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,27 @@
 import React from "react";
 import "./Navbar.css";
-import menu_icon from "../../assets/menu.png";
-import logo from "../../assets/logo.png";
-import search_icon from "../../assets/search.png";
-import upload_icon from "../../assets/upload.png";
-import more_icon from "../../assets/more.png";
-import notification_icon from "../../assets/notification.png";
-import profile_icon from "../../assets/user_profile.jpg";
+
+import {menu,logo,search,upload,more,notification,user_profile} from '../Files'
 
 const Navbar = () => {
   return (
     <nav className="flex-div">
       <div className="nav-left flex-div">
-        <img className="menu_icon" src={menu_icon} alt="" />
+        <img className="menu_icon" src={menu} alt="" />
         <img className="logo" src={logo} alt="" />
       </div>
       <din className="nav-middle flex-div">
         <div className="search-box flex-div">
 
         <input type="text" placeholder="Search" />
-        <img src={search_icon} alt="" />
+        <img src={search} alt="" />
         </div>
       </din>
       <div className="nav-right flex-div">
-        <img src={upload_icon} alt="" />
-        <img src={more_icon} alt="" />
-        <img src={notification_icon} alt="" />{" "}
-        <img className="user-icon" src={profile_icon} alt="" />
+        <img src={upload} alt="" />
+        <img src={more} alt="" />
+        <img src={notification} alt="" />{" "}
+        <img className="user-icon" src={user_profile} alt="" />
       </div>
     </nav>
   );
