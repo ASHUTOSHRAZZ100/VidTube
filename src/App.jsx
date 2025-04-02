6672d7c7f92db44a1e72c0f2421d9c109aa0e47a
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-02 17:00:40 +0530

    add Navbar component to App for improved layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-
-
+import Navbar from './Components/Navbar/Navbar'
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+    <Navbar/>
+    </div>
   )
 }
 
