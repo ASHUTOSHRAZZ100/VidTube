991034829da47209408ba70d67cc2c5c581cfac8
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-02 19:58:59 +0530

    add Feed component with card layout and styling

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,302 @@
+import React from "react";
+import "./Feed.css";
+import {
+  thumbnail1,
+  thumbnail2,
+  thumbnail3,
+  thumbnail4,
+  thumbnail5,
+  thumbnail6,
+  thumbnail7,
+  thumbnail8,
+} from "../Files";
+import { Link } from "react-router-dom";
+
+const Feed = () => {
+  return (
+    <div className="feed">
+      <Link to={`video/20/4521`} className="card">
+        <img src={thumbnail1} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </Link>
+      <div className="card">
+        <img src={thumbnail2} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail1} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail2} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail1} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail2} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail1} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail2} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail3} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail4} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail5} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail6} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail7} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+      <div className="card">
+        <img src={thumbnail8} alt="" />
+        <h2>
+          Best channel to learn codimg that help you to be a web developer
+        </h2>
+        <h3>Greatstack</h3>
+        <p>15k views &bull; 2 day ago</p>
+      </div>
+    </div>
+  );
+};
+
+export default Feed;
