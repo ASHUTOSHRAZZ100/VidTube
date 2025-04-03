6651b260e90980de95ac505a7efcb248ceee1d67
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-03 19:37:04 +0530

    refactor Recommended component: adjust layout and improve readability by reducing flex-basis and updating image alt attributes

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -22,83 +22,35 @@ const Recommended = () => {
     }
     return header;
   }
+  const arr = [1,2,3,4,5,6,7,8,9]
   return (
     <div className="recommended">
       <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
+        <img src={thumbnail1} alt="thumbnail-img" />
         <div className="vid-info">
           <h4>{checkHeaderLenght("Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?")}</h4>
           <p>Great Stack</p>
           <p >199k Views &bull; 3 days</p>
         </div>
-        <Link >
-        <img id="img-menu" src={dots} alt="" />
-        </Link>
+        <>
+        <img id="img-menu" src={dots} alt="dots-icon" />
+        </>
       </div>
-      <div className="side-video-list">
-        <img src={thumbnail2} alt="" />
+      {
+        arr.map((_,index)=>(
+          <div key={index} className="side-video-list">
+        <img src={thumbnail1} alt="thumbnail-img" />
         <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
+          <h4>{checkHeaderLenght("Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?")}</h4>
           <p>Great Stack</p>
-          <p>199k Views</p>
+          <p >199k Views &bull; 3 days</p>
         </div>
+        <>
+        <img id="img-menu" src={dots} alt="dots-icon" />
+        </>
       </div>
-      <div className="side-video-list">
-        <img src={thumbnail3} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail4} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail5} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail6} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail7} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail8} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="" />
-        <div className="vid-info">
-          <h4>Best Channel that help us to</h4>
-          <p>Great Stack</p>
-          <p>199k Views</p>
-        </div>
-      </div>
+        ))
+      }
     </div>
   );
 };
