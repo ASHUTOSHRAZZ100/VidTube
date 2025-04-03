9631eb17a522864d7072968eb6b6e1d1edd1e7dd
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-03 18:54:43 +0530

    add CreaterVideo to Profile component: include video listing in user profile layout

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Profile.css'
 import Creater from '../../Components/Creater/Creater'
 import Sidebar from '../../Components/Navbar/Sidebar/Sidebar'
+import CreaterVideo from '../../Components/Creater/CreaterVideo/CreaterVideo'
 const Profile = ({sidebar}) => {
   return (
     <div>
       <Sidebar sidebar={sidebar}/>
       <div className={`container ${sidebar?"":"large-container"}`}>
       <Creater/>
+      <CreaterVideo/>
       </div>
     </div>
   )
