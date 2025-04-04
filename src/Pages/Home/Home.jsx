386f21d44eff38df875779774163c39cc9cecc3f
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-05 05:06:11 +0530

    refactor Home component: add lightToggle prop for dark mode support, enhance layout structure, and update container styles

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,12 +3,17 @@ import "./Home.css";
 import Sidebar from "../../Components/Navbar/Sidebar/Sidebar";
 import Feed from "../../Components/Feed/Feed";
 
-const Home = ({sidebar}) => {
+const Home = ({ sidebar, lightToggle }) => {
+  console.log("light->", lightToggle);
   return (
     <>
-      <Sidebar sidebar={sidebar} />
-      <div className={`container ${sidebar?"":"large-container"}`}>
-        <Feed/>
+      <Sidebar sidebar={sidebar} lightToggle={lightToggle} />
+      <div
+        className={`container ${sidebar ? "" : "large-container"} ${
+          lightToggle ? "dark-container" : ""
+        }`}
+      >
+        <Feed lightToggle={lightToggle} />
       </div>
     </>
   );
