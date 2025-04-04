6edc7495de89441225f19f49239ac1dced10f318
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-05 05:06:31 +0530

    refactor Video component: implement dark mode support, enhance layout structure, and improve CSS styling

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -3,11 +3,12 @@ import "./video.css";
 import PlayVideo from "../../Components/PlayVideo/PlayVideo";
 import Recommended from "../../Components/Recommended/Recommended";
 
-const Video = () => {
+const Video = ({ lightToggle }) => {
+  console.log("light->", lightToggle);
   return (
-    <div className="play-container">
-      <PlayVideo />
-      <Recommended />
+    <div className={`play-container ${lightToggle ? "dark-mode" : ""}`}>
+      <PlayVideo lightToggle={lightToggle} />
+      <Recommended lightToggle={lightToggle} />
     </div>
   );
 };
