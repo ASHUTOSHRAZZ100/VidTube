9775d1b10fd52fd9f2c1fd686d42584d2fadf27d
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-05 02:41:10 +0530

    refactor Recommended component: enhance layout with dark mode support, improve navigation handling, and streamline thumbnail rendering

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -9,62 +9,70 @@ import {
   thumbnail6,
   thumbnail7,
   thumbnail8,
-  dots
+  dots,
 } from "../Files";
-import {Link} from 'react-router-dom'
+import { useNavigate } from "react-router-dom";
 
-const Recommended = () => {
+const Recommended = ({ lightToggle }) => {
+  const navigate = useNavigate();
 
-  const checkHeaderLenght = (header)=>{
+  const checkHeaderLenght = (header) => {
     header.trim();
-    if(header.length>60){
-     header= header.substring(0,60)+'...'
+    if (header.length > 60) {
+      header = header.substring(0, 60) + "...";
     }
     return header;
-  }
-  const arr = [thumbnail1,
+  };
+  const arr = [
+    thumbnail1,
     thumbnail2,
     thumbnail3,
     thumbnail4,
     thumbnail5,
     thumbnail6,
     thumbnail7,
-    thumbnail8,thumbnail1,
+    thumbnail8,
+    thumbnail1,
     thumbnail2,
     thumbnail3,
     thumbnail4,
     thumbnail5,
     thumbnail6,
     thumbnail7,
-    thumbnail8]
+    thumbnail8,
+  ];
   return (
-    <div className="recommended">
-      <div className="side-video-list">
-        <img src={thumbnail1} alt="thumbnail-img" />
-        <div className="vid-info">
-          <h4>{checkHeaderLenght("Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?")}</h4>
-          <p>Great Stack</p>
-          <p >199k Views &bull; 3 days</p>
+    <div className={`recommended ${lightToggle ? "dark-mode" : ""}`}>
+      {arr.map((Value, index) => (
+        <div
+          key={index}
+          className="side-video-list"
+          onClick={(e) => {
+            e.stopPropagation();
+            navigate(`/video/2/160`);
+          }}
+        >
+          <img className="video-thumbnail" src={Value} alt="thumbnail-img" />
+          <div className="vid-info">
+            <h4 className="video-title">
+              {checkHeaderLenght(
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?"
+              )}
+            </h4>
+            <p
+              className="video-channel-name"
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate(`/creater/126`);
+              }}
+            >
+              Great Stack
+            </p>
+            <p className="video-stats">199k Views &bull; 3 days</p>
+          </div>
+          <img className="video-menu-icon" src={dots} alt="dots-icon" />
         </div>
-        <>
-        <img id="img-menu" src={dots} alt="dots-icon" />
-        </>
-      </div>
-      {
-        arr.map((Value,index)=>(
-          <div key={index} className="side-video-list">
-        <img src={Value} alt="thumbnail-img" />
-        <div className="vid-info">
-          <h4>{checkHeaderLenght("Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?")}</h4>
-          <p>Great Stack</p>
-          <p >199k Views &bull; 3 days</p>
-        </div>
-        <>
-        <img id="img-menu" src={dots} alt="dots-icon" />
-        </>
-      </div>
-        ))
-      }
+      ))}
     </div>
   );
 };
