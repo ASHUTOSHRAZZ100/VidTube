a8036aecaab9ce51d99a6d544cbd405e8488ec44
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-05 05:06:00 +0530

    refactor Sidebar component: implement dark mode support, enhance layout structure, and improve CSS styling

--- src/Components/Navbar/Sidebar/Sidebar.jsx
+++ src/Components/Navbar/Sidebar/Sidebar.jsx
@@ -17,10 +17,13 @@ import {
   cameron,
 } from "../../Files";
 
-const Sidebar = ({sidebar}) => {
-  console.log(sidebar)
+const Sidebar = ({sidebar,lightToggle}) => {
   return (
-    <div className={`sidebar ${sidebar?"":"small-sidebar"}`}>
+    <div
+      className={`sidebar ${sidebar ? "" : "small-sidebar"} ${
+        lightToggle ? "dark-mode" : ""
+      }`}
+    >
       <div className="sortcut-links">
         <div className="side-link">
           <img src={home} alt="" />
@@ -61,28 +64,28 @@ const Sidebar = ({sidebar}) => {
         <hr />
       </div>
       <div className="subscribed-list">
-          <h3>Subscribed</h3>
-          <div className="side-link">
-            <img src={jack} alt="" />
-            <p>PewDiePie</p>
-          </div>
-          <div className="side-link">
-            <img src={simon} alt="" />
-            <p>MrBeast</p>
-          </div>
-          <div className="side-link">
-            <img src={tom} alt="" />
-            <p>Justin Bieber</p>
-          </div>
-          <div className="side-link">
-            <img src={cameron} alt="" />
-            <p>Nas Daily</p>
-          </div>
-          <div className="side-link">
-            <img src={megan} alt="" />
-            <p>5-Minute</p>
-          </div>
+        <h3>Subscribed</h3>
+        <div className="side-link">
+          <img src={jack} alt="" />
+          <p>PewDiePie</p>
         </div>
+        <div className="side-link">
+          <img src={simon} alt="" />
+          <p>MrBeast</p>
+        </div>
+        <div className="side-link">
+          <img src={tom} alt="" />
+          <p>Justin Bieber</p>
+        </div>
+        <div className="side-link">
+          <img src={cameron} alt="" />
+          <p>Nas Daily</p>
+        </div>
+        <div className="side-link">
+          <img src={megan} alt="" />
+          <p>5-Minute</p>
+        </div>
+      </div>
     </div>
   );
 };
