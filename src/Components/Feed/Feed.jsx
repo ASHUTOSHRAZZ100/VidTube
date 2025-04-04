520326109570aa1e64adcade88b9145713637bea
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-05 03:03:15 +0530

    refactor Feed component: add lightToggle prop for dark mode support, improve accessibility with alt text, and streamline layout

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -15,7 +15,7 @@ import {
 } from "../Files";
 import { useNavigate } from "react-router-dom";
 
-const Feed = () => {
+const Feed = ({ lightToggle }) => {
   const navigate = useNavigate();
 
   const checkHeaderLenght = (header) => {
@@ -54,54 +54,7 @@ const Feed = () => {
   ];
 
   return (
-    <div className="feed">
-    <div
-        className="card"
-        onClick={(e) => {
-          e.stopPropagation();
-          navigate(`/video/20/4020`);
-        }}
-      >
-        <img src={thumbnail1} alt="" />
-        <div className="thumbnail-creater">
-          <img
-            id="img-creater"
-            src={tom}
-            alt=""
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(`/creater/10`);
-            }}
-          />
-          <div>
-            <h2>
-              {checkHeaderLenght(
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni doloribus nisi omnis rem libero suscipit, esse mollitia culpa dolores minus itaque, quisquam tempora voluptas nesciunt consequuntur nobis dignissimos et quia?"
-              )}
-            </h2>
-            <p
-              id="p-link"
-              onClick={(e) => {
-                e.stopPropagation();
-                navigate(`/creater/10`);
-              }}
-            >
-              Greatstack <img id="p-link-approve-icon" src={approve} alt="" />
-            </p>
-            <p>15k views &bull; 2 day ago</p>
-          </div>
-          <img
-            id="img-menu"
-            src={dots}
-            alt=""
-            onClick={(e) => {
-              e.stopPropagation();
-              navigate(`/video/10/4580`);
-            }}
-          />
-        </div>
-      </div>  
-
+    <div className={`feed ${lightToggle ? "dark-feed" : ""}`}>
       {arr.map((value, index) => (
         <div
           key={index}
@@ -111,12 +64,12 @@ const Feed = () => {
             navigate(`/video/20/4020`);
           }}
         >
-          <img src={value} alt="" />
+          <img src={value} alt="Video thumbnail" />
           <div className="thumbnail-creater">
             <img
               id="img-creater"
               src={tom}
-              alt=""
+              alt="Creator profile"
               onClick={(e) => {
                 e.stopPropagation();
                 navigate(`/creater/10`);
@@ -142,7 +95,7 @@ const Feed = () => {
             <img
               id="img-menu"
               src={dots}
-              alt=""
+              alt="More options menu"
               onClick={(e) => {
                 e.stopPropagation();
                 navigate(`/video/10/4580`);
