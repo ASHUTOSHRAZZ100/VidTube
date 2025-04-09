be5aed524f0485d78e197ab47c6b6fa2442100bf
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-10 01:44:33 +0530

    feat: enhance Navbar component with icon integration and dark mode styles

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.css";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { logo, user_profile } from "../Files";
+import { ThemeContext } from "../../App";
 import {
-  menu,
-  logo,
-  search,
-  upload,
-  lightDark,
-  notification,
-  user_profile,
-} from "../Files";
+  CircleUser,
+  Menu,
+  Search,
+  SunMoon,
+  Bell,
+  Upload,
+} from "../../assets/Icons.FIle";
 
-const Navbar = ({
-  setSidebar,
-  setLightToggle,
-  lightToggle,
-  userDropdown,
-  setUserDropdown,
-}) => {
-
+const Navbar = () => {
   const navigate = useNavigate();
+  const {
+    lightToggle,
+    setLightToggle,
+    setSidebar,
+    userDropdown,
+    setUserDropdown,
+  } = useContext(ThemeContext);
+
+  const handleLogout = async () => {
+    try {
+      console.log("logout");
+      await axios.post(
+        `${import.meta.env.VITE_SERVER}/users/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+
+      // Clear user from context
+      console.log("logout1");
+      setUser(null);
+      console.log("logout2");
+
+      // Redirect to login or homepage
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
+
   return (
     <nav className={`flex-div ${lightToggle ? "dark-mode" : ""}`}>
       <div className="nav-left flex-div">
-        <img
-          className="menu_icon"
+        <Menu
+          className="icon menu_icon"
           onClick={() => setSidebar((prev) => !prev)}
-          src={menu}
-          alt="menu-icon"
         />
         <Link to={`/`}>
           <img className="logo" src={logo} alt="" />
@@ -36,28 +59,46 @@ const Navbar = ({
       <div className="nav-middle flex-div">
         <div className="search-box flex-div">
           <input type="text" placeholder="Search" />
-          <img src={search} alt="" />
+          <Search className="icon search-icon" onClick={() => {}} />
         </div>
       </div>
       <div className="nav-right flex-div">
-        <img src={upload} alt="upload-icon" />
-        <img src={notification} alt="notification-icon" />
-        <img
-          src={lightDark}
-          alt="toggle-icon"
+        {true && (
+          <>
+            <Upload className="icon upload-icon" />
+            <Bell className="icon bell-icon" />
+          </>
+        )}
+        <SunMoon
+          className="icon sun-moon-icon"
           onClick={() => setLightToggle((prev) => !prev)}
         />
-        <img
-          className="user-icon"
-          src={user_profile}
-          alt="user-icon"
-          onClick={() => setUserDropdown((prev) => !prev)}
-        />
-        <ul className={`user-dropdown ${userDropdown ? "active" : ""}`}>
-          <li onClick={() => navigate(`/login`)}>Login</li>
-          <li onClick={() => navigate("/settings")}>Settings</li>
-          <li onClick={() => navigate("/logout")}>Logout</li>
-        </ul>
+        {true ? (
+          <>
+            <img
+              className="user-icon"
+              src={user_profile}
+              alt="user-icon"
+              onClick={() => setUserDropdown((prev) => !prev)}
+            />
+            <ul className={`user-dropdown ${userDropdown ? "active" : ""}`}>
+              <li>Hello, {"Guest"}</li>
+              <li onClick={() => navigate(`/creater/${123}`)}>Profile</li>
+              <li onClick={() => navigate("/setting")}>Setting</li>
+              <li onClick={handleLogout}>Logout</li>
+            </ul>
+          </>
+        ) : (
+          <div
+            className="sign-button flex-div"
+            onClick={() => {
+              navigate(`/login`);
+            }}
+          >
+            <CircleUser className="circleUser" />
+            <p>SignIn</p>
+          </div>
+        )}
       </div>
     </nav>
   );
