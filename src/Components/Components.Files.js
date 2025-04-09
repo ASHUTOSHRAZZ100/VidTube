81a7478376316e994ea829fe5bc0b4d59219c2c5
ASHUTOSH RAJ <ashutoshrazz100@gmail.com>
2025-04-10 01:46:17 +0530

    feat: create Components.Files.js to export component modules refactor: clean up Files.js by removing unused asset imports

---
+++ src/Components/Components.Files.js
@@ -0,0 +1,23 @@
+import Recommended from "./Recommended/Recommended";
+import PlayVideo from "./PlayVideo/PlayVideo";
+import Navbar from "./Navbar/Navbar";
+import Sidebar from "./Navbar/Sidebar/Sidebar";
+import Feed from "./Feed/Feed";
+import Creater from "./Creater/Creater";
+import CreaterVideo from "./Creater/CreaterVideo/CreaterVideo";
+import AuthLayout from "./AuthLayout/AuthLayout";
+import Login from "../Components/AuthLayout/Form/Login/Login";
+import Register from "../Components/AuthLayout/Form/Register/Register";
+
+export {
+  Recommended,
+  PlayVideo,
+  Navbar,
+  Sidebar,
+  Feed,
+  Creater,
+  CreaterVideo,
+  AuthLayout,
+  Login,
+  Register
+};
